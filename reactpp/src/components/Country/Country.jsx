fc99247bba6320e0250881f93ea7e37229c39ec9
Asef Ahmed <asefahmed500@gmail.com>
2024-03-16 15:24:26 +0600

    updated

--- reactpp/src/components/Country/Country.jsx
+++ reactpp/src/components/Country/Country.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './Country.css';
 const Country = ({country}) => {
     // eslint-disable-next-line react/prop-types
-    const {name ,flags , population , area} = country;
+    const {name ,flags , population , area ,cca3} = country;
+
+    const [visited , setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited)
+    }
+
     return (
         <div className='country'>
            <h3>Name: {name?.common}</h3> 
            <img src={flags.png} alt="" />
            <p>Population : {population}</p>
            <p>Area : {area}</p>
+           <p><small>Code : {cca3}</small></p>
+           <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+           {visited ? 'I  have viisted this country ' : 'I want to visit '}
         </div>
     );
 };
--- reactpp/src/components/countries/Countries.jsx
+++ reactpp/src/components/countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
  
 const Countries = () => {
@@ -12,11 +13,13 @@ useEffect(()=>{
      .then(data => setCountries(data))
 },[])
     return (
-        <div>
+        <div >
             <h3>Countries: {countries.length}</h3>
+            <div className="country-container">
             {
                 countries.map(country => <Country key={country.cca3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
