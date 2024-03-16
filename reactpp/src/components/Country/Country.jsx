c2c2c49ef33dfed37b89d582f5ab6a06fcc2b98f
Asef Ahmed <asefahmed500@gmail.com>
2024-03-17 00:16:05 +0600

    done

--- reactpp/src/components/Country/Country.jsx
+++ reactpp/src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
+const Country = ({country , handleVisitedCOuntry , handleVisitedFlags}) => {
     // eslint-disable-next-line react/prop-types
     const {name ,flags , population , area ,cca3} = country;
 
@@ -11,13 +11,22 @@ const Country = ({country}) => {
         setVisited(!visited)
     }
 
+    // const PasswithParams = () => handleVisitedCOuntry(country)
+    
+
+    // console.log(handleVisitedCOuntry)
+
     return (
-        <div className='country'>
-           <h3>Name: {name?.common}</h3> 
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+           <h3 style={{color : visited ? 'purple' : 'white'}}>Name: {name?.common}</h3> 
            <img src={flags.png} alt="" />
            <p>Population : {population}</p>
            <p>Area : {area}</p>
            <p><small>Code : {cca3}</small></p>
+           <button onClick={() => handleVisitedCOuntry(country)} >Mark Visited </button>
+           <br />
+           <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag </button>
+           <br />
            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
            {visited ? 'I  have viisted this country ' : 'I want to visit '}
         </div>
--- reactpp/src/components/countries/Countries.jsx
+++ reactpp/src/components/countries/Countries.jsx
@@ -2,23 +2,67 @@ import { useEffect, useState } from "react";
 import Country from "../Country/Country";
 import './Countries.css'
 
- 
+
 const Countries = () => {
 
-const [countries , setCountries] = useState([]);
+    const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCOuntries] = useState([])
+    const [visitedFlag , setVisitedFlag] = useState([])
 
-useEffect(()=>{
-    fetch(`https://restcountries.com/v3.1/all`)
-     .then(res => res.json())
-     .then(data => setCountries(data))
-},[])
+
+    useEffect(() => {
+        fetch(`https://restcountries.com/v3.1/all`)
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+
+    const handleVisitedCOuntry = country => {
+        console.log('add this to ypur visited country ')
+        // console.log(country)
+        // visitedCountries.push(country)
+        const newVisitedCountries = [... visitedCountries , country ]
+        setVisitedCOuntries(newVisitedCountries)
+    }
+
+    const handleVisitedFlags = flag =>{
+        // console.log('flag adding ')
+        const newVsitedFlag = [... visitedFlag , flag]
+        setVisitedFlag(newVsitedFlag)
+    }
+
+
+
     return (
         <div >
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>VISITED COUNTRIES : {visitedCountries.length}</h5>
+                <ul>
+                     {
+                        visitedCountries.map(country =><li key={country.cca3} >{country.name.common}</li> )
+                     }
+                </ul>
+            </div>
+
+
+            <div className="flag-container ">
+                {
+                    visitedFlag.map((flag , idx) =>  <img key={idx} src = {flag}></img> )
+                }
+            </div>
             <div className="country-container">
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
+                {
+                    countries.map(country =>
+                        <Country
+                            key={country.cca3}
+                            country={country}
+                            handleVisitedCOuntry = {handleVisitedCOuntry}
+                            handleVisitedFlags = {handleVisitedFlags}
+                        >
+
+                        </Country>)
+                }
             </div>
         </div>
     );
