5df745cb9b4427829e5bdc23fc8ea1496197d12a
Asef Ahmed <asefahmed500@gmail.com>
2024-03-16 15:01:11 +0600

    updated

--- reactpp/src/components/Country/Country.jsx
+++ reactpp/src/components/Country/Country.jsx
@@ -2,11 +2,13 @@
 import './Country.css';
 const Country = ({country}) => {
     // eslint-disable-next-line react/prop-types
-    const {name ,flags} = country;
+    const {name ,flags , population , area} = country;
     return (
         <div className='country'>
            <h3>Name: {name?.common}</h3> 
            <img src={flags.png} alt="" />
+           <p>Population : {population}</p>
+           <p>Area : {area}</p>
         </div>
     );
 };
